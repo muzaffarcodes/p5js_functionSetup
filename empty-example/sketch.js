04af52665f3ed503eb5203a99852c5a01c78d13e
Muzaffar Khaydarov <114870653+muzaffarcodes@users.noreply.github.com>
2024-01-03 21:14:03 +0500

    Add files via upload

---
+++ empty-example/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // put setup code here
+  createCanvas(450,450);
+  background(255,0,0); // red
+}
+
+function draw() {
+  // put drawing code here
+  fill("cyan");
+  noStroke();
+  rect(225, 225, 200, 100, 30);
+}
